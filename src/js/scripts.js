a5bfb2e36d6555829da2290262ced4a6bbf4bb61
Elias-87 <elias.assefa@gmail.com>
2025-11-08 19:44:36 -0700

    Added comments

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,17 +2,17 @@
 async function main() {
   // This is where the code you're actually experimenting with goes.
 
-let tots = await input("Please enter your total cost of goods: ");
+let tots = await input("Please enter your total cost of goods: ");    // Collect total cost null = 0
 if (tots == null) {
   tots = 0
 }
-let moola = await input("Please enter your total cash payment: ");
+let moola = await input("Please enter your total cash payment: ");    // Collect Cash payment, null = 0
 if (moola == null) {
   tots = 0
 }
-let disco = await input("Please enter discount code: ");
+let disco = await input("Please enter discount code: ");              // Collects discount code, null = 0
 
-switch (disco) {
+switch (disco) {                                                      // Switch case applies discount code to total cost, null = 0
   case null:
     tots = tots * 1;
     break;
@@ -35,21 +35,21 @@ switch (disco) {
     tots = tots* 0.6;
     break;
   default:
-    output("Invalid Entry");
+    output("Invalid Entry");                                           // Default Invalid Entry for any input other than 0 - 5
 }
 
-const formatter = new Intl.NumberFormat('en-US', {
+const formatter = new Intl.NumberFormat('en-US', {                     // Formats integer to currency
   style: 'currency',
   currency: 'USD'
 });
 
-finTots = moola - tots;
-finTotsmoney = formatter.format(finTots)
+finTots = moola - tots;                                                // Calculates change
+finTotsmoney = formatter.format(finTots)                               // Converts change to currency
 
-if (finTots >= 0){
+if (finTots >= 0){                                                     // If payment is sufficient, outputs message with change in currency
   output("Your change is " + finTotsmoney + ".")
 } else{
-  output("Insufficient Payment")
+  output("Insufficient Payment")                                       // Else if change is less than 0, outputs Insufficient payment message
 }
 }   
 
